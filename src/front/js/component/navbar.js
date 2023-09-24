a1973a66ecd57ec3a417e7fde6e6a812c4183ec2
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-09-24 13:03:37 +0000

    pensando en un bocadillo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import  "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+		<header>
+		
+		<nav className="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+		  <div className="container">
+		
+			<button className="navbar-toggler" type="button" data-mdb-toggle="collapse"
+			  data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+			  aria-label="Toggle navigation">
+			  <i className="fas fa-bars"></i>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+			  <ul className="navbar-nav me-auto">
+				<li className="nav-item">
+				  <a className="nav-link" href="#!">Mis proyectos</a>
+				</li>
+				<li className="nav-item">
+				  <a className="nav-link" href="#!">Features</a>
+				</li>
+				<li className="nav-item">
+				  <a className="nav-link" href="#!">Portfolio</a>
+				</li>
+				<li className="nav-item">
+				  <a className="nav-link" href="#!">Reference</a>
+				</li>
+				<li className="nav-item">
+				  <a className="nav-link" href="#!">About</a>
+				</li>
+				<li className="nav-item">
+				  <a className="nav-link" href="#!">Team</a>
+				</li>
+				<li className="nav-item">
+				  <a className="nav-link" href="#!">Contact</a>
+				</li>
+			  </ul>
+			  <ul className="navbar-nav d-flex flex-row">
+				<li className="nav-item me-3 me-lg-0">
+				  <a className="nav-link" href="#!">
+					<i className="fas fa-shopping-cart"></i>
+				  </a>
+				</li>
+				<li className="nav-item me-3 me-lg-0">
+				  <a className="nav-link" href="#!">
+					<i className="fab fa-twitter"></i>
+				  </a>
+				</li>
+				<li className="nav-item me-3 me-lg-0">
+				  <a className="nav-link" href="#!">
+					<i className="fab fa-instagram"></i>
+				  </a>
+				</li>
+			  </ul>
 			</div>
+		  </div>
 		</nav>
+		
+		
+		
+	  </header>
 	);
 };
